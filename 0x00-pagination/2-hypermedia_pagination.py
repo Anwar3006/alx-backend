36f0bb53920ca5316a5e2b0fb503fe65e8c815b1
Anwar3006 <anwarsadat.d2@gmail.com>
2023-05-15 11:08:44 +0000

    Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -56,23 +56,22 @@ class Server:
         dataset = self.dataset()
 
         new_list = []
-        for data in dataset[page1[0] : page1[1]]:
+        for data in dataset[page1[0]:page1[1]]:
             new_list.append(data)
         return new_list
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """
-        
+        returns a dictionary containing key-value pairs:
         """
         pageSize = 0 if page > math.floor(19419 / page_size) else page_size
         contentSize = 19419 / page_size
         return {
-            'page_size' : pageSize,
-            'page' : page,
-            'data' : [] if pageSize == 0 else self.get_page(page, pageSize),
-            'next_page' : None if pageSize == 0 else page + 1,
-            'prev_page' : None if page - 1 == 0 else page - 1,
-            'total_pages' : math.ceil(contentSize)
+            'page_size': pageSize,
+            'page': page,
+            'data': [] if pageSize == 0 else self.get_page(page, pageSize),
+            'next_page': None if pageSize == 0 else page + 1,
+            'prev_page': None if page - 1 == 0 else page - 1,
+            'total_pages': math.ceil(contentSize)
             if pageSize == 0 else math.floor(contentSize)
         }
-    
